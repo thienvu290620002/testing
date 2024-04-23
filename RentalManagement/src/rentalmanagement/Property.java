a466783cbab8ff49406e05c3bf12e265c09a57fd
thienvu290620002 <167847428+thienvu290620002@users.noreply.github.com>
2024-04-23 14:48:32 +0700

    Add files via upload

---
+++ RentalManagement/src/rentalmanagement/Property.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rentalmanagement;
+
+/**
+ *
+ * @author Asus
+ */
+public class Property {
+    private String propertyID;
+    private String description;
+    private PropertyOwner propertyOwner;
+
+    public Property(String propertyID, String description, PropertyOwner propertyOwner) {
+        this.propertyID = propertyID;
+        this.description = description;
+        this.propertyOwner = propertyOwner;
+    }
+
+    public String getPropertyID() {
+        return propertyID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public PropertyOwner getPropertyOwner() {
+        return propertyOwner;
+    }
+    
+    @Override
+    public String toString() {
+        return "Property ID: " + propertyID + ", Description: " + description + ", Property Owner: " + propertyOwner.getUser().getUserID();
+    }
+    
+}
